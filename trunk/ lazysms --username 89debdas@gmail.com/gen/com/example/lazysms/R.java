654d5d6931a63cb8fb128ba1a104278d4bbb1203
89debdas@gmail.com <89debdas@gmail.com@689f8a7b-98f8-9975-bd90-d8c74ab75229>
2014-07-02 12:21:57 +0000

    Language Translator and SMS Backup added..

--- trunk/ lazysms --username 89debdas@gmail.com/gen/com/example/lazysms/R.java
+++ trunk/ lazysms --username 89debdas@gmail.com/gen/com/example/lazysms/R.java
@@ -8,7 +8,17 @@
 package com.example.lazysms;
 
 public final class R {
+    public static final class array {
+        public static final int lang=0x7f040001;
+        public static final int max_items_per_sync_entries=0x7f040000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int status_done=0x7f050001;
+        public static final int status_error=0x7f050003;
+        public static final int status_idle=0x7f050000;
+        public static final int status_sync=0x7f050002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,65 +27,172 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int custom_btn_orange=0x7f020000;
         public static final int gradientshape=0x7f020001;
         public static final int home=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logi1=0x7f020004;
-        public static final int login_background=0x7f020005;
-        public static final int rec_text_edit=0x7f020006;
-        public static final int red_button=0x7f020007;
-        public static final int rounded_corner=0x7f020008;
-        public static final int spencers_logo=0x7f020009;
+        public static final int ic_done=0x7f020003;
+        public static final int ic_error=0x7f020004;
+        public static final int ic_idle=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_launcher_smssync=0x7f020007;
+        public static final int ic_launcher_smssync2=0x7f020008;
+        public static final int ic_menu_update=0x7f020009;
+        public static final int ic_syncing=0x7f02000a;
+        public static final int logi1=0x7f02000b;
+        public static final int login_background=0x7f02000c;
+        public static final int rec_text_edit=0x7f02000d;
+        public static final int red_button=0x7f02000e;
+        public static final int rounded_corner=0x7f02000f;
+        public static final int spencers_logo=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080019;
-        public static final int btnDraft=0x7f080008;
-        public static final int btnInbox=0x7f080001;
-        public static final int btnSelectContact=0x7f080016;
-        public static final int btnSendSMS=0x7f080013;
-        public static final int btnSentBox=0x7f080007;
-        public static final int btnSpeak=0x7f080017;
-        public static final int btnWriteSms=0x7f080000;
-        public static final int btntexttovoice=0x7f080002;
-        public static final int btnvoicetotext=0x7f080003;
-        public static final int button1=0x7f080005;
-        public static final int editText1=0x7f08000e;
-        public static final int editText2=0x7f080011;
-        public static final int lblMsg=0x7f08000b;
-        public static final int lblNumber=0x7f08000c;
-        public static final int linearLayout1=0x7f080006;
-        public static final int lvMsg=0x7f080009;
-        public static final int msg_id=0x7f08000a;
-        public static final int tableRow1=0x7f08000f;
-        public static final int tableRow2=0x7f080012;
-        public static final int tableRow3=0x7f080014;
-        public static final int tableRow4=0x7f080015;
-        public static final int tableRow5=0x7f08000d;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080010;
-        public static final int txtText=0x7f080018;
+        public static final int about_content=0x7f0b0000;
+        public static final int action_settings=0x7f0b002a;
+        public static final int btnDraft=0x7f0b0011;
+        public static final int btnInbox=0x7f0b0002;
+        public static final int btnSelectContact=0x7f0b001f;
+        public static final int btnSendSMS=0x7f0b001c;
+        public static final int btnSentBox=0x7f0b0010;
+        public static final int btnSpeak=0x7f0b0020;
+        public static final int btnWriteSms=0x7f0b0001;
+        public static final int btnsmsbackup=0x7f0b0005;
+        public static final int btntexttovoice=0x7f0b0003;
+        public static final int btntranslate=0x7f0b0006;
+        public static final int btnvoicetotext=0x7f0b0004;
+        public static final int button1=0x7f0b000e;
+        public static final int details_error=0x7f0b0029;
+        public static final int details_sync=0x7f0b0025;
+        public static final int details_sync_label=0x7f0b0028;
+        public static final int details_sync_progress=0x7f0b0026;
+        public static final int details_sync_progress_indet=0x7f0b0027;
+        public static final int editText1=0x7f0b0017;
+        public static final int editText2=0x7f0b001a;
+        public static final int etUserText=0x7f0b0009;
+        public static final int lblMsg=0x7f0b0014;
+        public static final int lblNumber=0x7f0b0015;
+        public static final int linearLayout1=0x7f0b000f;
+        public static final int lvMsg=0x7f0b0012;
+        public static final int msg_id=0x7f0b0013;
+        public static final int say=0x7f0b000b;
+        public static final int selectLanguage=0x7f0b000a;
+        public static final int status_icon=0x7f0b0022;
+        public static final int status_label=0x7f0b0024;
+        public static final int sync_button=0x7f0b0023;
+        public static final int tableRow1=0x7f0b0018;
+        public static final int tableRow2=0x7f0b001b;
+        public static final int tableRow3=0x7f0b001d;
+        public static final int tableRow4=0x7f0b001e;
+        public static final int tableRow5=0x7f0b0016;
+        public static final int text=0x7f0b0007;
+        public static final int textView1=0x7f0b000d;
+        public static final int textView2=0x7f0b0019;
+        public static final int translatedtext=0x7f0b0008;
+        public static final int tvTranslatedText=0x7f0b000c;
+        public static final int txtText=0x7f0b0021;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int message_details=0x7f030001;
-        public static final int messagebox=0x7f030002;
-        public static final int row=0x7f030003;
-        public static final int sendsms=0x7f030004;
-        public static final int speech_to_text=0x7f030005;
-        public static final int text_to_voice=0x7f030006;
+        public static final int about_dialog=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int backup_sms=0x7f030002;
+        public static final int language_translate=0x7f030003;
+        public static final int message_details=0x7f030004;
+        public static final int messagebox=0x7f030005;
+        public static final int row=0x7f030006;
+        public static final int sendsms=0x7f030007;
+        public static final int speech_to_text=0x7f030008;
+        public static final int status=0x7f030009;
+        public static final int text_to_voice=0x7f03000a;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f0a0000;
+    }
+    public static final class plurals {
+        public static final int status_done_details=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int about_email_button=0x7f070042;
+        public static final int about_email_content=0x7f070044;
+        public static final int about_email_subject=0x7f070043;
+        public static final int about_email_uri=0x7f070045;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int app_version=0x7f070003;
+        public static final int choose_lang=0x7f070048;
+        public static final int err_communication_error=0x7f07003e;
+        public static final int err_could_not_create_folder=0x7f07003b;
+        public static final int err_first_sync_needs_skip_flag=0x7f07003c;
+        public static final int err_sync_requires_login_info=0x7f07003d;
+        public static final int hello_world=0x7f070002;
+        public static final int menu_info=0x7f07003f;
+        public static final int menu_market=0x7f070041;
+        public static final int menu_share=0x7f070040;
+        public static final int share_body=0x7f070047;
+        public static final int share_subject=0x7f070046;
+        public static final int status_auth_failure=0x7f070015;
+        public static final int status_auth_failure_details=0x7f070016;
+        public static final int status_calc=0x7f070017;
+        public static final int status_calc_details=0x7f070018;
+        public static final int status_canceled=0x7f070019;
+        public static final int status_canceled_details=0x7f07001a;
+        public static final int status_done=0x7f070010;
+        public static final int status_done_details_max_per_sync=0x7f070011;
+        public static final int status_done_details_noitems=0x7f070012;
+        public static final int status_idle=0x7f070009;
+        public static final int status_idle_details=0x7f07000a;
+        public static final int status_idle_details_never=0x7f07000b;
+        public static final int status_login=0x7f07000e;
+        public static final int status_login_details=0x7f07000f;
+        public static final int status_sync=0x7f07000c;
+        public static final int status_sync_details=0x7f07000d;
+        public static final int status_unknown_error=0x7f070013;
+        public static final int status_unknown_error_details=0x7f070014;
+        public static final int ui_dialog_first_sync_msg=0x7f070036;
+        public static final int ui_dialog_first_sync_title=0x7f070035;
+        public static final int ui_dialog_invalid_imap_folder_msg=0x7f070034;
+        public static final int ui_dialog_invalid_imap_folder_title=0x7f070033;
+        public static final int ui_dialog_missing_credentials_msg=0x7f070030;
+        public static final int ui_dialog_missing_credentials_title=0x7f07002f;
+        public static final int ui_dialog_need_first_manual_sync_msg=0x7f070038;
+        public static final int ui_dialog_need_first_manual_sync_title=0x7f070037;
+        public static final int ui_dialog_sync_data_reset_msg=0x7f070032;
+        public static final int ui_dialog_sync_data_reset_title=0x7f070031;
+        public static final int ui_enable_auto_sync_desc=0x7f07002e;
+        public static final int ui_enable_auto_sync_label=0x7f07002d;
+        public static final int ui_imap_folder_desc=0x7f070027;
+        public static final int ui_imap_folder_label=0x7f070026;
+        public static final int ui_imap_folder_label_dialog_msg=0x7f070028;
+        public static final int ui_login_desc=0x7f070021;
+        public static final int ui_login_dialog_msg=0x7f070022;
+        public static final int ui_login_label=0x7f070020;
+        public static final int ui_mark_as_read_desc=0x7f07002c;
+        public static final int ui_mark_as_read_label=0x7f07002b;
+        public static final int ui_max_items_per_sync_desc=0x7f07002a;
+        public static final int ui_max_items_per_sync_label=0x7f070029;
+        public static final int ui_password_desc=0x7f070024;
+        public static final int ui_password_dialog_msg=0x7f070025;
+        public static final int ui_password_label=0x7f070023;
+        public static final int ui_settings_advanced_cat_label=0x7f07001f;
+        public static final int ui_settings_advanced_desc=0x7f07001e;
+        public static final int ui_settings_advanced_label=0x7f07001d;
+        public static final int ui_settings_label=0x7f07001c;
+        public static final int ui_skip=0x7f07003a;
+        public static final int ui_status_label=0x7f07001b;
+        /**  string name="ui_ok">OK</string 
+ string name="ui_yes">Yes</string 
+ string name="ui_no">No</string 
+         */
+        public static final int ui_sync=0x7f070039;
+        public static final int ui_sync_button_label_canceling=0x7f070008;
+        public static final int ui_sync_button_label_done=0x7f070005;
+        public static final int ui_sync_button_label_error=0x7f070006;
+        public static final int ui_sync_button_label_idle=0x7f070004;
+        /**  TODO(chstuder): Rename the following string resource. 
+         */
+        public static final int ui_sync_button_label_syncing=0x7f070007;
     }
     public static final class style {
         /** 
@@ -99,13 +216,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int CustomTitleBarTheme=0x7f060003;
-        public static final int button_text=0x7f060002;
-        public static final int titleBarBackgroundStyle=0x7f060004;
+        public static final int AppTheme=0x7f090001;
+        public static final int CustomTitleBarTheme=0x7f090003;
+        public static final int button_text=0x7f090002;
+        public static final int titleBarBackgroundStyle=0x7f090004;
     }
 }
